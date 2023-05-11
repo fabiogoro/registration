3bde9fa86b214d12fd68962ffdb5522885ae7276
Fabio Gorodscy <fabiogo@outlook.com>
2023-05-11 09:53:38 -0300

    Code cleaning

--- inputmanager.js
+++ inputmanager.js
@@ -1,77 +1,75 @@
+/**
+ * This is an UI manager class. 
+ * This is linked to a HTMLElement through 'input' argument.
+ * It uses the storage manager to set the element initial value with cached values.
+ * Also, it updates localStorage, using storage manager, by setting onblur event.
+ * It requires the HTMLElement id to match the stored value in the localStorage.
+ * @constructor
+ * @param {HTMLElement} input - the HTMLElement controlled by this instance. It is expected a text like input, because it sets the value with cached value.
+ *
+ */
 class InputManager{
-  constructor(input, storage){
+  static selector = ".input-storage"
+  
+  constructor(input){
     this.input = input
-    this.storage = storage
     this.input.onblur = this.onblur.bind(this)
     this.updateValue()
   }
 
   updateValue(){
-    if(this.storage.user[this.input.id]) 
-      this.input.value = this.storage.user[this.input.id]
+    if(localStorageManager.user[this.input.id]) 
+      this.input.value = localStorageManager.user[this.input.id]
   }
 
   onblur(){
-    this.storage.user[this.input.id] = this.input.value
-    this.storage.save()
+    localStorageManager.user[this.input.id] = this.input.value
+    localStorageManager.save()
+  }
+
+  static createInstances(){
+    const elements = document.querySelectorAll(this.selector)
+    elements.forEach((i)=>new this(i))
   }
 }
 
+/**
+ * This is an UI manager class. This extends InputManager with functionality to work on checkboxes. 
+ * This is linked to a HTMLElement through 'input' argument.
+ * It uses the storage manager to set the element initial value with cached values.
+ * Also, it updates localStorage, using storage manager, by setting onchange event, since onblur is different for checkboxes.
+ * It requires the HTMLElement name attribute to match the stored value in the localStorage, and will create a list of strings from the checkbox values.
+ * @constructor
+ * @param {HTMLElement} input - the HTMLElement controlled by this instance. It is expected a checkbox like input, because it sets the checked property.
+ *
+ */
 class CheckboxManager extends InputManager{
-  constructor(input, storage){
-    super(input, storage)
+  static selector = ".checkbox-storage"
+  
+  constructor(input){
+    super(input)
     this.input.onchange = this.onchange.bind(this)
   }
 
   updateValue(){
     this.name = this.input.getAttribute('name')
-    if(!this.storage.user[this.name]) {
-      this.storage.user[this.name] = []
+    if(!localStorageManager.user[this.name]) {
+      localStorageManager.user[this.name] = []
     } else 
-      this.input.checked = this.storage.user[this.name].includes(this.input.value)
+      this.input.checked = localStorageManager.user[this.name].includes(this.input.value)
   }
 
   onchange(){
-    if(this.storage.user[this.name].includes(this.input.value))
-      this.storage.user[this.name] = this.storage.user[this.name].filter(item => item !== this.input.value)
+    if(localStorageManager.user[this.name].includes(this.input.value))
+      localStorageManager.user[this.name] = localStorageManager.user[this.name].filter(item => item !== this.input.value)
     else
-      this.storage.user[this.name].push(this.input.value)
-    this.storage.save()
+      localStorageManager.user[this.name].push(this.input.value)
+    localStorageManager.save()
   }
 
   onblur(){
   }
 }
 
-class DisplayManager {
-  constructor(container, storage){
-    this.container = container
-    this.storage = storage
-    this.startContainer()
-  }
-
-  startContainer(){
-    if(this.storage.user[this.container.id])
-      this.container.innerText += ` ${this.storage.user[this.container.id]}`
-  }
-
-}
-
-class DisplayImageManager extends DisplayManager{
-  startContainer(){
-    if(this.storage.user[this.container.id])
-      this.container.firstChild.src = this.storage.user[this.container.id]
-  }
-}
-
-class DisplayAddressManager extends DisplayManager{
-  startContainer(){
-    const city = this.storage.user['city']
-    const street = this.storage.user['street']
-    const house = this.storage.user['house']
-    if(city && street && house)
-      this.container.innerText += ` ${house}, ${street}, ${city}`
-    else if(city && street)
-      this.container.innerText += ` ${street}, ${city}`
-  }
-}
+InputManager.createInstances()
+CheckboxManager.createInstances()
--- pageredirection.js
+++ pageredirection.js
@@ -1,3 +1,9 @@
+/**
+ * This controls page access for this application. 
+ * It saves the current page and checks if it matches with the savedPage using the storage manager.
+ * It publishes a function for page redirection, which redirects and updates savedPage in cache.
+ *
+ */
 class PageManager{
   constructor(){
     this.currentLocation = window.location.pathname.split('/').pop()
